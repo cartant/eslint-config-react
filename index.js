ea6b3b01fe1cae4d50f91743e1fa5a8e7f5fc459
Nicholas Jamieson <nicholas@cartant.com>
2021-06-12 10:28:03 +1000

    chore: Enable no-unstable-nested-components.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 module.exports = {
   plugins: ["react", "react-etc", "react-hooks"],
   rules: {
+    "react/no-unstable-nested-components": ["error"],
     "react-etc/prefer-usememo": ["warn"],
     "react-hooks/rules-of-hooks": ["error"],
-    "react-hooks/exhaustive-deps": ["error"]
+    "react-hooks/exhaustive-deps": ["error"],
   },
 };
