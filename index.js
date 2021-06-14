35fdab2100eae6efcf27d69aaae9e9bb490b3a9b
Nicholas Jamieson <nicholas@cartant.com>
2021-06-14 21:17:53 +1000

    chore: Enable no-unstable-context-selector.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: ["react", "react-etc", "react-hooks"],
   rules: {
     "react/no-unstable-nested-components": ["error"],
+    "react-etc/no-unstable-context-selector": ["error"],
     "react-etc/prefer-usememo": ["warn"],
     "react-hooks/rules-of-hooks": ["error"],
     "react-hooks/exhaustive-deps": ["error"],
